0301b80f617e7f3c63c2515ad4c821cbac7b1396
Donald Chow <donald7290@gmail.com>
2024-05-28 14:44:09 +0900

    contained streams

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -9,6 +9,6 @@ class Quote < ApplicationRecord
   # after_update_commit -> { broadcast_replace_later_to "quotes" }
   # after_destroy_commit -> { broadcast_remove_to "quotes" }
   # replace with broadcasts_to
-  broadcasts_to -> (quote) { :quotes }, inserts_by: :prepend
+  broadcasts_to -> (quote) { [quote.company, "quotes"] }, inserts_by: :prepend
 
 end
