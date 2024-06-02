57b4683536a9a0481a87bb5f527b5c94cd22abef
Donald Chow <donald7290@gmail.com>
2024-06-02 18:44:51 +0900

    added test to line_item

--- test/system/line_item_dates_test.rb
+++ test/system/line_item_dates_test.rb
@@ -1,4 +1,4 @@
-require "application_system_test_case"
+require 'application_system_test_case'
 
 class LineItemDatesTest < ApplicationSystemTestCase
   setup do
@@ -10,38 +10,39 @@ class LineItemDatesTest < ApplicationSystemTestCase
     visit quote_path(@quote)
   end
 
-  test "create a line item date" do
-    assert_selector "h1", text: "First quote"
+  test 'create a line item date' do
+    assert_selector 'h1', text: 'First quote'
 
-    click_on "New date"
-    assert_selector "h1", text: "First quote"
-    fill_in "Date", with: Date.current + 1.day
+    click_on 'New date'
+    assert_selector 'h1', text: 'First quote'
+    fill_in 'Date', with: Date.current + 1.day
 
-    click_on "Create date"
+    click_on 'Create date'
     assert_text I18n.l(Date.current + 1.day, format: :long)
   end
 
-  test "update a line item date" do
-    assert_selector "h1", text: "First quote"
+  test 'update a line item date' do
+    assert_selector 'h1', text: 'First quote'
 
-    within id: dom_id(@line_item_date) do
-      click_on "Edit"
+    within id: dom_id(@line_item_date, :edit) do
+      click_on 'Edit'
     end
 
-    assert_selector "h1", text: "First quote"
+    assert_selector 'h1', text: 'First quote'
 
-    fill_in "Date", with: Date.current + 1.day
-    click_on "Update date"
+    fill_in 'Date', with: Date.current + 1.day
+    click_on 'Update date'
 
     assert_text I18n.l(Date.current + 1.day, format: :long)
   end
 
-  test "delete a line item date" do
-    assert_selector "h1", text: "First quote"
+  test 'delete a line item date' do
+    assert_selector 'h1', text: 'First quote'
+    assert_text I18n.l(Date.current, format: :long)
 
-    within id: dom_id(@line_item_date) do
-      accept_confirm do
-        click_on "Delete"
+    accept_confirm do
+      within id: dom_id(@line_item_date, :edit) do
+        click_on 'Delete'
       end
     end
 
